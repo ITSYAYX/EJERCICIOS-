1cfebce06be47f772a63f2eec7eae82d50f5bcb0
ITSYAYX <159969172+ITSYAYX@users.noreply.github.com>
2024-05-20 18:58:54 -0500

    Update Arreglo-bidimensional.c
    Arreglo

--- Arreglo-bidimensional.c
+++ Arreglo-bidimensional.c
@@ -5,23 +5,22 @@ int main() {
     int numeroBuscado;
     int i, j;
 
-    // Llenar la matriz con números secuenciales
+  
     for (i = 0; i < 9; i++) {
         for (j = 0; j < 9; j++) {
             matriz[i][j] = i * 9 + j + 1;
         }
     }
 
-    // Pedir al usuario que ingrese un número para buscar
+
     printf("Ingrese un numero para buscar en la matriz: ");
     scanf("%d", &numeroBuscado);
 
-    // Buscar el número en la matriz
     for (i = 0; i < 9; i++) {
         for (j = 0; j < 9; j++) {
             if (matriz[i][j] == numeroBuscado) {
                 printf("Numero %d encontrado en la posicion (%d, %d)\n", numeroBuscado, i, j);
-                return 0; // Termina el programa después de encontrar el número
+                return 0; 
             }
         }
     }
