d6b55b75e803f54f7d3fa522d74b53badabd52ad
ITSYAYX <159969172+ITSYAYX@users.noreply.github.com>
2024-05-20 18:59:24 -0500

    Update Arreglo-unidimensional.c
    arreglo

--- Arreglo-unidimensional.c
+++ Arreglo-unidimensional.c
@@ -2,20 +2,20 @@
 
 int main() {
     int arreglo[88];
-    int numeroInicial = 1; // Puedes cambiar este número para iniciar desde otro valor
+    int numeroInicial = 1; 
     int numeroBuscado;
     int encontrado = 0;
 
-    // Llenar el arreglo con números secuenciales a partir de numeroInicial
+   
     for (int i = 0; i < 88; i++) {
         arreglo[i] = numeroInicial + i;
     }
 
-    // Pedir al usuario que ingrese un número para buscar
+   
     printf("Ingrese un numero para buscar en el arreglo: ");
     scanf("%d", &numeroBuscado);
 
-    // Buscar el número en el arreglo
+  
     for (int i = 0; i < 88; i++) {
         if (arreglo[i] == numeroBuscado) {
             printf("Valor encontrado en la posicion %d\n", i);
